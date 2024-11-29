c00a116272a09dbbc228c40b5ee6826a7459770b
stavanger91 <afeinberg4@gmail.com>
2024-11-29 10:36:35 +0100

    ...

--- scripts.js
+++ scripts.js
@@ -387,9 +387,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Separate regex for English and Japanese sentence splitting
     const englishSentenceEndings =
       /(?<!\b(?:Mr|Mrs|Ms|Mt|Dr|Prof|Sr|St|Jr|Lt|Gen|Col|Capt|Cmdr|Sgt)\.)(?<=[.!?:]["”]?)\s+(?=["”]?[A-Z])/g;
-    const japaneseSentenceEndings =
-      /(?<=[。！？」](?<!」(?=\s*と))|[」](?!\s*と)[？！」])/g;
-
+    const japaneseSentenceEndings = /(?<=[。！？])」?(?!と)|(?<=。)」/g;
     // Function to split English sentences
     function splitEnglishSentences(text) {
       const standardizedText = text.replace(/[“”«»]/g, '"'); // Standardize quotes
